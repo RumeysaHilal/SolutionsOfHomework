da5f2eabe4453daaeec546d25688f9e2f651e590
= <=>
2021-01-24 02:26:09 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi: " + musteri.Ad + "  " + musteri.Soyad+"\n");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi: " + musteri.Ad + "  " + musteri.Soyad+"\n");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            Console.WriteLine("Müşteriler :");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Ad Soyad: " + musteri.Ad + " " + musteri.Soyad + "  Id: " + musteri.Id);
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Musteri musteri1 = new Musteri();
+                musteri1.Ad = "Hilal";
+                musteri1.Soyad = "Sevinç";
+                musteri1.Id = 1;
+                musteri1.Tcno = "1111111111";
+
+                Musteri musteri2 = new Musteri();
+                musteri2.Ad = "Rümeysa";
+                musteri2.Soyad = "Sevinç";
+                musteri2.Id = 2;
+                musteri2.Tcno = "12222222222";
+
+                Musteri musteri3 = new Musteri();
+                musteri3.Ad = "Rümeysa Hilal";
+                musteri3.Soyad = "Sevinç";
+                musteri3.Id = 3;
+                musteri3.Tcno = "133333333333";
+
+                Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+                MusteriManager manager = new MusteriManager();
+                manager.Ekle(musteri1);
+                manager.Sil(musteri2);
+                manager.Listele(musteriler);
+            }
+        }
+    }
